e2fd9864e5d0a38db615289414d722e124feccfe
sid <sid@sids-MacBook-Air.local>
2022-01-07 00:16:45 +0530

    Initial commit

---
+++ src/main/java/org/example/Bootstrap.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import io.vertx.core.AbstractVerticle;
+
+public class Bootstrap extends AbstractVerticle {
+
+    @Override
+    public void start() {
+        System.out.println("this is new i dont know dfdsf what to do");
+    }
+}
